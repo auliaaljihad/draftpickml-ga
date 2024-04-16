e021aa0397dba42993b4f3c54357b28540753fe0
auliaaljihad <jiadaulia@gmail.com>
2024-04-16 22:30:14 +0700

    baru

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-
-st.write("# Welcome to Our Project! 👋")
-
-st.sidebar.success("Select a demo above.")
-
+st.title('Genetic Algorithm MLBB for Draft Pick Mythic Rank')
 st.write('Presented by:')
 st.write(
 'Aulia Al-Jihad Safhadi, ',
 'M. Shafri Syamsuddin, ',
 'Naafi Rofiiqoh Makaarimah')
+
+st.title('🎈 Presentation Deck')
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSe1PgrGeOWIH6RU-kp6OmGSa59hAiW-9kz5YZWAhvh_p_yT_TEZ53k1rUh5vNKI8UXXhEtFS8eoAlO/embed?start=false&loop=false&delayms=3000", height=570)
